2dddb0496729fa291336489dbc98fba7c28821ad
seaeast2 <haedong.lee@att.net>
2017-06-01 22:28:09 +0000

    align test src

--- test_parser.cpp
+++ test_parser.cpp
@@ -1,10 +1,12 @@
 #include "lexer.h"
 #include "parser.h"
 
-char* src =  " import test.aaa.bbb.ccc; \n\
-              void test() {\n\
-                int a = 10;\n\
-              }";
+char* src =  
+" \
+import test.aaa.bbb.ccc; \n\
+void test() {\n\
+  int a = 10;\n\
+}";
 
 int main() {
   Tokenizer* tk = new Tokenizer(src);
