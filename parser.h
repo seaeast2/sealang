1894735d74bdbec190bfb194fa4bba609a8193d7
seaeast2 <haedong.lee@att.net>
2018-01-19 02:46:49 +0000

    remove hangul coments

--- parser.h
+++ parser.h
@@ -7,13 +7,9 @@
 
 using namespace Lexer;
 
-// 원칙 : 함수 내부에서 무언가 오류가 있을 때는 false 리턴
-// 제대로 문법 범위 내에서 끝났으면 true 리턴
-// todo : false 를 리턴 할 때 Error 의 종류를 파악 할 수 있는
-// 추가적인 구조 생성
-namespace Parser {
-
+namespace Parser {  
   class SyntaxAnalyzer {
+
     //Tokenizer* tokenizer_;
     WordChecker* wc_;
 
