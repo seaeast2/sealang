eb2d0a7b1586ecad22cc306afde8dfd4d62a3910
seaeast2 <haedong.lee@att.net>
2018-03-25 01:32:36 -0500

    add linked list functions

--- core/list.h
+++ core/list.h
@@ -12,20 +12,82 @@ class SimpleList {
   };
 
   private:
-  struct Data {
     Item* first_;
     Item* last_;
-  };
-  Data* data_;
+    int count_;
 
   public:
-    SimpleList() { data_ = NULL; }
-    ~SimpleList() {}
+    SimpleList() { 
+      first_ = last_ = NULL; 
+      count_ = 0;
+    }
+    ~SimpleList() {
+    }
 
-    void Add(const T& value) {
-      if (data_ == NULL) {
-        data_ = new Data;
+    Item* AddTail(const T& value) {
+      Item* new_item = CreateNewItem(value);
+
+      if (!first_) {
+        first_ = last_ = new_item;
       }
+      else {
+        new_item->prev_ = last_;
+        last_->next_ = new_item;
+        last_ = new_item;
+      }
+
+      count_++;
+      return new_item;
+    }
+
+    Item* Find(const T& value) {
+      Item* itr = first_;
+      while(itr->next_) {
+        if (itr->value == value)
+          return itr;
+        itr = itr->next_;
+      }
+      return NULL;
+    }
+
+    Item* GetAt(int idx) {
+      if (idx > count_)
+        return NULL;
+
+      Item* itr = first_;
+      for (int cnt = 0; cnt < idx; cnt++,itr = itr->next_) {
+      }
+
+    }
+
+    // insert at index position.
+    Item* InsertAt(int idx, const T& value) {
+      if (idx > count_)
+        return NULL;
+
+      Item* new_item = CreateNewItem(value);
+
+      return NULL;
+    }
+
+    Item* Front() {
+      return first_;
+    }
+
+    Item* Back() {
+      return last_;
+    }
+
+    int GetSize() {
+      return count_;
+    }
+
+  private:
+    Item* CreateNewItem(const T& value) {
+      Item* new_item = new Item;
+      new_item->value = value;
+      new_item->priv_ = new_item->next_ = NULL;
+      return new_item;
     }
 
 };
