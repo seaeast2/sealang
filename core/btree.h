7b89b03388aecaeb0a9b62a096fdce50c4d4f1e1
seaeast2 <haedong.lee@att.net>
2018-04-17 20:35:28 -0500

    btree.h

--- core/btree.h
+++ core/btree.h
@@ -4,13 +4,15 @@
 // binary tree
 template <class K, class V> 
 class BTree {
-  struct Node {
+  public:
+  struct BtNode {
     K key_;
     V value_;
-    Node* left_, *right_;
+    BtNode* parent_, *left_, *right_;
   };
 
-  Node* root_;
+  private:
+  BtNode* root_;
 
   public:
     BTree() {
@@ -20,7 +22,66 @@ class BTree {
     ~BTree() {
     }
 
-    // clear();
+    bool AddNode(const K& key, const V& value) {
+      BtNode* newnode = new BtNode;
+      newnode->key_ = key;
+      newnode->value_ = value;
+      newnode->parent_ = NULL;
+      newnode->left_ = NULL;
+      newnode->right_ = NULL;
+
+      if (root_ == NULL) {
+        root_ = newnode;
+      }
+      else {
+        BtNode* focus_node = root_;
+        BtNode* parent = NULL;
+        while(true) {
+          parent = focus_node;
+          if (key < focus_node->key) {
+            focus_node = focus_node->left_;
+            if (focus_node == NULL) {
+              parent->left_ = newnode;
+              newnode->parent_ = parent;
+              return true;
+            }
+          }
+          else if (key == focus_node->key_) {
+            // duplicate key. 
+            return false;
+          }
+          else {
+            focus_node = focus_node->right_;
+            if (focus_node == NULL) {
+              parent->right_= newnode;
+              newnode->parent_ = parent;
+              return true;
+            }
+          }
+        }
+      }
+      assert("Binary Tree Node adding error");
+      return false;
+    }
+
+
+    BtNode* FindNode(const K& key) {
+      BtNode* focus_node = root_;
+
+      while(focus_node->key_ != key) {
+        if (key < focus_node->key) {
+          focus_node = focus_node->left_;
+        }
+        else {
+          focus_node = focus_node->right_;
+        }
+      }
+
+      return focus_node;
+    }
+
+    /*BtItr* Begin();
+    BtItr* End();*/
 };
 
 #endif
--- core/stack.h
+++ core/stack.h
@@ -60,7 +60,91 @@ class SimpleArrayStack {
     T& GetAt(unsigned int index) {
       if (index < STACK_SIZE) 
         return data_[index];
+      return data_[top_];
     }
+};
+
+
+// resizable array stack 
+template <class T> 
+class RSStack{
+  enum {
+    ST_ASSIGN_SIZE = 8,
+  };
+
+  T* data_;
+  int stack_size_;
+  int top_;
+
+  public:
+  RSStack() {
+    data_ = NULL;
+    stack_size_ = 0;
+    top_ = -1;
+  }
+  // copy constructor
+  RSStack(const RSStack &st) {
+    for (int i = 0; i < st.TopIndex()+1; i++) {
+      Push(st.GetAt(i));
+    }
+  }
+
+  ~RSStack() {
+    Clear();
+  }
+
+  void Push(const T& t) {
+    if (stack_size_ < top_+2) {
+      T* new_data = new T[stack_size_ + ST_ASSIGN_SIZE];
+      stack_size_ += ST_ASSIGN_SIZE;
+      // copy items
+      Copy(new_data, data_, top_+1);
+      delete[] data_;
+      data_ = new_data;
+    }
+
+    top_++;
+    *(data_+top_) = t;
+  }
+
+  int Pop() {
+    if (top_ < 0)
+      return top_;
+
+    return top_--;
+  }
+
+  T& Top() { 
+    return data_[top_];
+  }
+
+  int TopIndex() {
+    return top_;
+  }
+  
+  bool IsEmpty() {
+    if (top_ < 0)
+      return true;
+    return false;
+  }
+
+  void Clear() {
+    delete[] data_;
+    stack_size_ = 0;
+    top_ = -1;
+  }
+
+  void Copy(T* dest, T* src, int size) {
+    for (int i = 0; i < size; i++) {
+      *(dest+i) = *(src+i);
+    }
+  }
+  
+  T& GetAt(unsigned int index) {
+    if (index < stack_size_) 
+      return data_[index];
+    return data_[top_];
+  }
 };
 
 #endif
