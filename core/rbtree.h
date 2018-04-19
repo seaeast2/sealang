505d5fb6229eb9dd019ccc513c863f3e8f2cd782
seaeast2 <haedong.lee@att.net>
2018-04-18 19:35:18 -0500

    red black tree

--- core/rbtree.h
+++ core/rbtree.h
@@ -21,12 +21,26 @@ class RBTree {
       eColor color_;
       RbNode* parent_, *left_, *right_;
     };
+
+  private:
+    RbNode* root_;
+    RbNode* nil_leaf_; // sentinel leaf node
+
   public:
     RBTree() {
+      root_ = NULL;
+      nil_leaf_ = new RbNode;
+      nil_leaf_->color_ = BLACK;
+      nil_leaf_->parent_ = NULL;
+      nil_leaf_->left = NULL;
+      nil_leaf_->right_ = NULL;
     }
 
     ~RBTree() {
+      delete nil_leaf_;
     }
+
+    //bool RotateLeft(
 };
 
 
