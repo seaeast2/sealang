4a9048a6b9f5160cfc77205ed9caef56c81dc7e8
seaeast2 <seaeast2@gmail.com>
2018-07-18 02:51:57 -0500

    add sample makefile

---
+++ module.mk
@@ -0,0 +1,15 @@
+# Sample nonrecusive make module.mk
+#
+
+local_dir := lib/codec
+local_lib := $(local_dir)/libcodec.a
+local_src := $(addprefix $(local_dir)/,codec.c)
+local_objs := $(subst .c,.o,$(local_src)
+
+libraries += $(local_lib)
+sources += $(local_src)
+
+$(local_lib): $(local_objs)
+	$(AR) $(ARFLAGS) $@ $^
+
+
--- utils.mk
+++
@@ -1,4 +0,0 @@
-# Nonrecusive Makefile sample
-
-
-
