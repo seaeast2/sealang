310c3048e473fdf988eb787dc2238f271b252db0
seaeast2 <haedong.lee@att.net>
2018-09-16 07:50:52 -0500

    add ir_codegen.cpp

---
+++ src/ir1_codegen.cpp
@@ -0,0 +1,20 @@
+#include "ir1_codegen.h"
+
+using namespace IR1;
+
+bool CodeGen::GenerateIR(Declarations* decl) {
+
+  return true;
+}
+
+bool CodeGen::VisitStmt(StmtNode* nodes) {
+  return true;
+}
+
+bool CodeGen::VisitExpr(ExprNode* node) {
+  return true;
+}
+
+bool Codegen::Visit(IfNode* node) {
+  return true;
+}
